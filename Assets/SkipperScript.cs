6e1d40caecaf8b558ea3353d556a9698f440f8c7
John McDonald <jcpmcdonald@gmail.com>
2014-09-09 20:55:51 -0600

    Adding google ads, getting the fish to animate, and getting the fish to react to gravity

---
+++ Assets/SkipperScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkipperScript : MonoBehaviour {
+
+	private float acceleration = -4;
+	private float velocity = 4;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		velocity += acceleration * Time.deltaTime;
+		this.transform.position.Set(transform.position.x, transform.position.y + velocity * Time.deltaTime, 0);
+	}
+}
